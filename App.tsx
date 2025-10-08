69f44d425b65eed105934f37422ebe25b647708c
Nasim Ahmad <khannasimahmad@gmail.com>
2025-10-08 09:06:02 +0530

    changed the styling imported various components regisstred the headless task

--- App.tsx
+++ App.tsx
@@ -1,44 +1,35 @@
-import { Alert, StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-import StockJumpp from './components/StockJumpp';
-import { useEffect } from 'react';
-import { initBackgroundScheduler } from './webScraper/utility/backgroundScheduler';
-
-
-
+import React, { useEffect } from 'react';
+import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from './components/AppNavigator';
+import { headlessTask, initBackgroundScheduler } from './webScraper/utility/backgroundScheduler';
+import BackgroundFetch from 'react-native-background-fetch';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
+  useEffect(() => {
+    // Initialize the background scheduler once when app starts
+    initBackgroundScheduler()
+    // Register the headless task (so it runs when app is killed)
+    BackgroundFetch.registerHeadlessTask(headlessTask);
+  }, []);
+
   return (
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+      <View style={styles.container}>
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      </View>
     </SafeAreaProvider>
   );
 }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-  // Initialize Node.js immediately when the app process starts
-  useEffect(() => {
-    initBackgroundScheduler();
-  }, []);
-  
-  return (
-    <View style={styles.container}>
-      <StockJumpp />
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+  container: { flex: 1 },
 });
 
 export default App;
