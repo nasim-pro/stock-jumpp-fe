9a8f9b808680eaec1af3096ea6b24aaac5a65c44
Nasim Ahmad <khannasimahmad@gmail.com>
2025-10-09 07:24:01 +0530

    registered the background fetch

--- index.js
+++ index.js
@@ -5,5 +5,11 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
+import BackgroundFetch from 'react-native-background-fetch';
+import { headlessTask } from './webScraper/utility/backgroundScheduler';
+
 
 AppRegistry.registerComponent(appName, () => App);
+
+// Register headless task globally (outside React)
+BackgroundFetch.registerHeadlessTask(headlessTask);
