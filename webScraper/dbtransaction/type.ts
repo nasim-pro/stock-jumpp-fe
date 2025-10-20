75634a6e60dde923c557f6a6ca0130f1663ed14a
Nasim Ahmad <khannasimahmad@gmail.com>
2025-10-20 07:50:03 +0530

    added types

--- webScraper/dbtransaction/type.ts
+++ webScraper/dbtransaction/type.ts
@@ -1,7 +1,7 @@
 
  interface GrowthRate {
-    oldGrowthRate: number;
-    newGrowthRate: number;
+    oldGrowthRate?: number;
+    newGrowthRate?: number;
     impliedValue?: number;
     jumpPercent?: number;
     change?: number;
@@ -10,6 +10,8 @@
 interface Recommendation {
     EPS: GrowthRate;
     Sales: GrowthRate;
+    OP: GrowthRate;
+    PAT: GrowthRate;
     PEG: number;
     decision: "BUY" | "SELL" | "HOLD" | string; // fallback string for unknown cases
     [key: string]: any;
@@ -25,14 +27,20 @@ export interface StockData {
     promoterHolding: number;
     roe: number;
     roce: number;
+
     quarters: number[];
     quarterlySales: number[];
     quarterlyPat: number[];
     quarterlyEps: number[];
     quarterlyOpProfit: number[];
+
     years: number[];
     yearlySales: number[];
     yearlyEps: number[];
+    yearlyOpProfit: number[];
+    yearlyPat: number[];
+    
+
     recommendation: Recommendation;
     DPS: number;
 }
